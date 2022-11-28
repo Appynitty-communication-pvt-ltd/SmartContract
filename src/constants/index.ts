2b6ac8bc4bd8d01e7aa2655f4d6ded092bf5513d
Saurabh Lodha <saurabhlodha221b@gmail.com>
2022-11-28 04:32:32 +0300

    Removed unrequired files after latest smart contract updates and NestJs structure updates

--- src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-export const RPC_LINK = 'https://rpc-mumbai.maticvigil.com/';
-export const TRIP_CONTRACT_ADDRESS =
-  '0xe3E97741c0cED8E0C698946b192B618DAd365E52';
-export const GARBAGE_VERIFICATION_STATUS = {
-  '0': 'UNINITIALIZED : ',
-  '1': 'PENDING',
-  '2': 'SUCCESS',
-  '3': 'FAILED: ',
-};
--- src/smartcontracts/smartcontracts.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('smartcontracts')
-export class SmartcontractsController {}
