3d4f2f0445ec9359707d01ec62345c742b291ed7
Saurabh Lodha <saurabhlodha221b@gmail.com>
2022-11-28 04:34:50 +0300

    Updated dtos and interfaces and completed logic for smart contract integration

---
+++ src/trips/dtos/trips.dto.ts
@@ -0,0 +1,14 @@
+export class AddTripDataDto {
+  tripId: string;
+  transId: string;
+  startDateTime: string;
+  endDateTime: string;
+  userId: string;
+  dyId: string;
+  houseList: Array<string>;
+  tripNo: string;
+  vehicleNumber: string;
+  totalGcWeight: string;
+  totalDryWeight: string;
+  totalWetWeight: string;
+}
--- src/trips/trips.controller.ts
+++ src/trips/trips.controller.ts
@@ -1,45 +1,26 @@
 import { Controller, Post, Get, Param, Body, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { TripsService } from './trips.service';
-import {
-  AddHouseHoldAggreegateWasteDto,
-  AddDumpyardWasteDto,
-} from './trips.dto';
+import { AddTripDataDto } from './dtos/trips.dto';
+
 @Controller('trips')
+@UseGuards(AuthGuard('local'))
 export class TripsController {
   constructor(private tripsService: TripsService) {}
-  @UseGuards(AuthGuard('local'))
-  @Post('/:tripid/households')
-  async addHouseHoldAggregatedData(
-    @Param('tripid') tripid: string,
-    @Body() addHouseHoldAggreegateWasteDto: AddHouseHoldAggreegateWasteDto,
+
+  @Post('/:tripId/tripdata')
+  public async addTripData(
+    @Param('tripId') tripId: string,
+    @Body() addTripDataDto: AddTripDataDto,
   ) {
-    const wasteParams = {
-      ...addHouseHoldAggreegateWasteDto,
-      tripId: tripid,
-    };
-    const addHouseHoldAggregatedData =
-      await this.tripsService.addHouseHoldAggregatedData(wasteParams);
-    return addHouseHoldAggregatedData;
+    return this.tripsService.addTripData({
+      ...addTripDataDto,
+      tripId,
+    });
   }
-  @UseGuards(AuthGuard('local'))
-  @Post('/:tripid/dumpyard')
-  async addDumpyardWasteData(
-    @Param('tripid') tripid: string,
-    @Body() addDumpyardWasteDto: AddDumpyardWasteDto,
-  ) {
-    const wasteParams = {
-      ...addDumpyardWasteDto,
-      tripId: tripid,
-    };
-    const addDumpyardWasteData = await this.tripsService.addDumpyardWasteData(
-      wasteParams,
-    );
-    return addDumpyardWasteData;
-  }
-  @UseGuards(AuthGuard('local'))
-  @Get('/:tripid')
-  getVrificationStatus(@Param('tripid') tripid: string) {
-    return this.tripsService.getVrificationStatus(tripid);
+
+  @Get('/:tripId')
+  public async getVerificationStatus(@Param('tripId') tripId: string) {
+    return this.tripsService.getVerificationStatus(tripId);
   }
 }
